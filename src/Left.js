fd5b32e8f1c173aad3355e030671aceb1b955d2c
satya-9 <palaparthisatyanarayana.iit@gmail.com>
2021-03-31 22:27:28 +0530

    final

--- src/Left.js
+++ src/Left.js
@@ -4,13 +4,9 @@ export default function Left({data,i,toggle}) {
     return (
         <div className={"data" + (data.open ? "openr" : "" )}
         key={i} onClick={()=>toggle(i)}>
-            <div className={"data-question"+(data.open ? "open" : "" )}>{data.question}
-            <div className={"data-matter"+ (data.open ? "open" : "" )}>{data.matter}</div>
+                        <div className={"data-matter"+ (data.open ? "open" : "" )}>{data.matter}</div>
+            <div className={"data-question"+(data.open ? "open" : "" )}>{data.question}</div>
 
-            
-            </div>            
-            
-
-        </div>
+            </div>
     )
 }
